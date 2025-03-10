3cfe9b45fb99dc694e6701d2f5a28a3051dc4e26
Adam Green <adam@bristlebird.com>
2025-03-10 09:13:50 +0000

    add amazon S3 static file hosting config for deployment

---
+++ custom_storages.py
@@ -0,0 +1,13 @@
+"""
+Custom Storages for Amazon S3 static & media file hosting
+"""
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class StaticStorage(S3Boto3Storage):
+    location = settings.STATICFILES_LOCATION
+
+
+class MediaStorage(S3Boto3Storage):
+    location = settings.MEDIAFILES_LOCATION
