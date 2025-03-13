c0d32a152f39b72c69262d23d713e4b6472c0e05
Adam Green <adam@bristlebird.com>
2025-03-13 18:10:16 +0000

    update 500 error page, remove commented code from templates, pep8 fixes

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
+"""
+Urls for the home page app!
+"""
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,16 @@
+"""
+Home page app views
+"""
 from django.shortcuts import render
-
-# Create your views here.
 
 
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+# def homepage_view(request):
+#     """ A view used to test 500 error page """
+
+#     raise Exception("This is a test error")
